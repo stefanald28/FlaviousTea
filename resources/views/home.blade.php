a38219a37e5968b82548569e4f54e7454b2cc6b6
stefanald28 <stefanald28@gmail.com>
2021-10-28 03:17:07 +0700

    a

---
+++ resources/views/home.blade.php
@@ -0,0 +1,37 @@
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Home | Flavious Tea</title>
+    <link rel="icon" type="image/png" href="img/FlaviousTea.png">
+    <link rel="stylesheet" href="{{asset('css/app.css')}}">
+</head>
+<body>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <a class="navbar-brand" href=""><img src="img/FlaviousTea.png" width="50"> <b> Flavious Tea </b></a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+          <ul class="navbar-nav">
+            <li class="nav-item active">
+              <a class="nav-link" href="">Home <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="">Prodak</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Pricing</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+</body>
+</html>
---
+++ resources/views/list.blade.php
@@ -0,0 +1,10 @@
+<div class="card ml-3 mb-3" style="width: 150px; height: 300px;">
+    <a href="" class="text-dark">
+        <div class="badge bg-dark text-white position-absolute" style="top: 0.5rem; right: 0.5rem"></div>
+        <img class="card-img-top" src="img/FlaviousTea.png" alt="FlaviousTea" style="width: 150px; height: 200px;">
+        <div class="card-body">
+            <h5 class="card-title text-center">Flavious Tea</h5>
+
+        </div>
+    </a>
+</div>
